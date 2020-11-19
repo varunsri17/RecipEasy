4900bc06c10b46d3b7795054969c75b2e9b8dd85
Hmasood97 <hisham9977@gmail.com>
2020-11-18 23:29:17 -0500

    password stuff

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const user = require('./user.js')
 
 const bodyParser = require('body-parser');
 
+const expressSession = require('express-session')
+
+app.use(expressSession( {
+    name: "eazySessionCookie",
+    secret: "express eazy secret",
+    resave: false,
+    saveUnitialized: false,
+    cookie: { secure: false }
+}));
+
 app.use(bodyParser.json());
 //hello
 //apparently this is very unsafe and we need a better way to this
@@ -34,17 +44,82 @@ const getRecipeInfoURL = 'https://api.spoonacular.com/recipes/{id}/information'
 // Get request URL for autocomplete search to suggest impartial searches
 const autoCompleteURL = 'https://api.spoonacular.com/recipes/autocomplete'
 
+
+//
+const login_info = require('data-store')({ path: process.cwd() + '/data/users.json' });
+//test
+//const login_info = require('data-store')({ path: process.cwd() + '/data/users.json' });
+
+
+//LOGIN CALLS
+app.post('/login', (req,res) => {
+
+    let user = req.body.user;
+    let password = req.body.password;
+
+    let user_info = login_info.get(user);
+    if (user_info == null) {
+        res.status(404).send("Not found")
+        return;
+    }
+
+    if (user_info.password == password) {
+        console.log("VALID CREDENTIALS" + ' ' + user)
+        req.session.user = user;
+        res.json(true);
+        return;
+    }
+    res.status(403).send("UNAUTHORIZED USER THIS IS THE FBI")
+    
+})
+
+//LOGOUT CALL
+app.get('/logout', (req,res) => {
+    delete req.session.user;
+    res.json(true);
+})
+
+//USER CALLS
+
+//get all usernames
+app.get('/usernames', async(req, res, next) => {
+    res.json(user.getAllUserNames());
+    
+});
+
+// see if there is someone with this username
+app.get('/usernames/:username', async(req, res, next) => {
+    let u = user.findByUsername(req.params.username)
+
+    if (u ==null) {
+        res.status(200).send("No user with this username: " + req.params.username );
+        return;
+    }
+    res.status(409).send('USER WITH USERNAME: ' + req.params.username + ' FOUND')
+    
+});
+
+
 // get all user
+// This wont require authentication so we can get all infos sent without password
+// or we can add authentication and then use our cookie to get in
 app.get('/user', async(req, res, next) => {
     res.json(user.getAllIDs());
 
 });
-
-// get user by id
+// get user data by id
 app.get('/user/:id', (req,res) => {
+    if(req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
     let u = user.findByID(req.params.id);
     if (u == null) {
-        res.status(404).send("User not found");
+        res.status(404).send("User not found using user.findbyId");
+        return;
+    }
+    if(u.username!= req.session.user) {
+        res.status(403).send("Unauthorized")
         return;
     }
     res.json(u);
@@ -52,8 +127,8 @@ app.get('/user/:id', (req,res) => {
 
 // create user information
 app.post('/user', cors(), (req, res) => {
-    let {firstName, lastName, favorites, diet} = req.body;
-    let u = user.create(firstName, lastName, favorites, diet);
+    let {firstName, lastName, favorites, diet } = req.body;
+    let u = user.create(username,firstName, lastName, favorites, diet);
     if (u == null) {
         res.status(404).send("Bad Request");
     }
@@ -62,12 +137,20 @@ app.post('/user', cors(), (req, res) => {
 
 // update user information
 app.put('/user/:id', cors(), (req, res) => {
-    let u = user.findByID(req.params.id);
-    if (u == null) {
-        res.status(404).send("User not found");
+    if(req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
         return;
     }
-    let {firstName, lastName, favorites, diet} = req.body;
+    let u = user.findByID(req.params.id);
+    if (u == null) {
+        res.status(404).send("User not found using user.findbyId");
+        return;
+    }
+    if(u.username!= req.session.user) {
+        res.status(403).send("Unauthorized")
+        return;
+    }
+    let {firstName, lastName, favorites, diet,} = req.body;
     u.firstName = firstName;
     u.lastName = lastName;
     u.favorites = favorites;
@@ -78,9 +161,17 @@ app.put('/user/:id', cors(), (req, res) => {
 
 // delete user information
 app.delete('/user/:id', cors(), (req, res) => {
+    if (req.session.user == undefined) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
     let u = user.findByID(req.params.id);
     if (u == null) {
         res.status(404).send("User not found");
+        return;
+    }
+    if(u.username!= req.session.user) {
+        res.status(403).send("Unauthorized")
         return;
     }
     u.delete();
--- user.js
+++ user.js
@@ -1,8 +1,9 @@
-const user_data = require('data-store')({ path: process.cwd() + '/data/user.json' });
+const user_data = require('data-store')({ path: process.cwd() + '/data/cooks.json' });
 
 class user {
-    constructor(id, firstName, lastName, favorites, diet) {
+    constructor( id, username, firstName, lastName, favorites, diet) {
         this.id = id;
+        this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.favorites = favorites;
@@ -18,8 +19,8 @@ class user {
     }
 }
 
-let u1 = new user(0, "Emily", "Junior", [1,2], 0);
-user_data.set(u1.id.toString(), u1);
+//let u1 = new user(0, "emily1", "Emily", "Junior", [1,2], 0);
+//user_data.set(u1.id.toString(), u1);
 
 //return an array of all user ids
 user.getAllIDs = () => {
@@ -29,8 +30,11 @@ user.getAllIDs = () => {
 //return user information by id
 user.findByID = (id) => {
     let udata =  user_data.get(id);
-    if (udata == null) return null;
-    return new user(udata.id, udata.firstName, udata.lastName, udata.favorites, udata.diet);
+    if (udata == null) { 
+    console.log("no user with this ID"); 
+    return null;
+    }
+    return new user( udata.id, udata.username, udata.firstName, udata.lastName, udata.favorites, udata.diet);
 }
 
 user.next_id = user.getAllIDs().reduce((max, next_id) => {
@@ -41,12 +45,36 @@ user.next_id = user.getAllIDs().reduce((max, next_id) => {
 }, -1) + 1;
 
 //create a user given parameters
-user.create = (firstName, lastName, favorites, diet) => {
+user.create = (username, firstName, lastName, favorites, diet) => {
     let id = user.next_id;
     user.next_id +=1;
-    let u = new user(id, firstName, lastName, favorites, diet);
+    let u = new user(id, username, firstName, lastName, favorites, diet);
     user_data.set(u.id.toString(), u);
     return u;
 }
+
+//get all usernames so we can make sure no username is taken 
+const username_data = require('data-store')({ path: process.cwd() + '/data/users.json' });
+
+// get all usernames
+user.getAllUserNames = () => {
+    console.log('in the all userNames function')
+    return Object.keys(username_data.data).map((username => {return username;}));
+}
+
+
+user.findByUsername = (username) => {
+    let namedata = username_data.get(username);
+    if (namedata == null) { 
+        console.log("no user with this username, creation good to go"); 
+        return null;
+    }
+    console.log("USER WITH THIS NAME EXISTS")
+    return namedata;
+}
+
+
+
+
 
 module.exports = user;
